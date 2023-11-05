25d9e7c1383a0c3231788f0ef298a5c3deaa0661
ponponnsan <2469suhara@gmail.com>
2023-11-05 09:21:53 +0900

    delete comment out

--- src/contracts/test/BraGo.ts
+++ src/contracts/test/BraGo.ts
@@ -1,50 +1,3 @@
-// import { expect } from 'chai';
-// import { ethers } from 'hardhat';
-// import { Contract, Signer } from 'ethers';
-
-// describe('BraGo Contract', function () {
-//   let BraGo: Contract;
-//   let braGo: Contract;
-//   let owner: Signer;
-//   let addr1: Signer;
-//   let addrs: Signer[];
-
-//   beforeEach(async function () {
-//     // Get the ContractFactory and Signers here.
-//     const braGo= await ethers.deployContract("BraGo");
-//     [owner, addr1, ...addrs] = await ethers.getSigners();
-//     // Deploy a new BraGo contract before each test.
-//     await braGo.waitForDeployment();
-//   });
-
-//   describe('Deployment', function () {
-//     it('Should set the right owner', async function () {
-//       expect(await braGo.owner()).to.equal(await owner.getAddress());
-//     });
-
-//     it('Should mint initial supply to the owner', async function () {
-//       const ownerBalance = await braGo.balanceOf(await owner.getAddress());
-//       expect(ownerBalance).to.equal(ethers.parseUnits('1000000', await braGo.decimals()));
-//     });
-//   });
-
-//   describe('Minting', function () {
-//     it('Should mint new tokens', async function () {
-//       const mintAmount = ethers.parseUnits('100', await braGo.decimals());
-//       await braGo.connect(owner).tranfer(await addr1.getAddress(), mintAmount, '0', '0', 'Test Message');
-//       const addr1Balance = await braGo.balanceOf(await addr1.getAddress());
-//       expect(addr1Balance).to.equal(mintAmount);
-//     });
-
-//     it('Should emit Transfer event on mint', async function () {
-//       const mintAmount = ethers.parseUnits('100', await braGo.decimals());
-//       await expect(braGo.connect(owner).transfer(await addr1.getAddress(), mintAmount, '0', '0', 'Test Message'))
-//         .to.emit(braGo, 'Transfer')
-//         .withArgs(await addr1.getAddress(), mintAmount, '0', '0', 'Test Message');
-//     });
-//   });
-// });
-
 import { expect } from 'chai';
 import { ethers, ContractFactory, Contract } from 'hardhat';
 
@@ -75,13 +28,7 @@ describe('BraGo Contract', function () {
       const ownerBalance = await braGo.balanceOf(await owner.getAddress());
       expect(ownerBalance).to.equal(ethers.parseUnits('100000', await braGo.decimals()));
     });
-    // it('Should mint initial supply to the owner', async function () {
-    //   const decimals = await braGo.decimals();
-    //   const expectedBalance = ethers.parseUnits('100000', decimals);
-    //   const ownerBalance = await braGo.balanceOf(await owner.getAddress());
-    //   expect(ownerBalance.toString()).to.equal(expectedBalance.toString());
-    // });
-    
+
   });
 
   describe('Minting', function () {
