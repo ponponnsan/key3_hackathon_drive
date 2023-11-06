f97eb1762040ff891ca9bdbb8866cd533e62c35d
ponponnsan <2469suhara@gmail.com>
2023-11-06 13:05:11 +0900

    deploy to mumbai

--- src/contracts/hardhat.config.ts
+++ src/contracts/hardhat.config.ts
@@ -7,7 +7,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
 const ACCOUNT_PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 console.log("PrivateKey set:", !!ACCOUNT_PRIVATE_KEY)
-
+const ALCHEMY_API_KEY_MUMBAI= process.env.ALCHEMY_API_KEY
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
@@ -15,8 +15,13 @@ const config: HardhatUserConfig = {
     artifacts: "./src",
   },
   networks: {
-      zKatana: {
-        url: `https://rpc.zkatana.gelato.digital`,
+      hardhat: {},
+      // zKatana: {
+      //   url: `https://rpc.zkatana.gelato.digital`,
+      //   accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      // },
+      maticmum: {
+        url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY_MUMBAI}`,
         accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
       },
   },
