2e213d8f2db89267a701b5f56e2c869105298987
cardene <chotips08@gmail.com>
2023-11-12 09:47:20 +0900

    💫 gerato & safe aa 対応

--- src/app/app/common/config.ts
+++ src/app/app/common/config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const PROJECT_ID = process.env.PROJECT_ID as string;
 export const ZERO_DEV_CUSTODIAL_PRIVATE_KEY = process.env
   .ZERO_DEV_CUSTODIAL_PRIVATE_KEY as string;
@@ -8,4 +11,6 @@ export const ZERO_DEV_CUSTODIAL_KEY_ID = process.env
 export const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
 export const OWNER_PUBLIC_KEY = process.env.OWNER_PUBLIC_KEY as `0x${string}`;
 
-export const GELATO_PROVIDER = "https://rpc.zkatana.gelato.digital";
+export const PROVIDER_URL = "https://rpc.zkatana.gelato.digital";
+export const CONTRACT_ADDRESS = "0x9743e736159786Fe49Bf89b6D0Fb56c227a41608";
+export const RECEIVE_TOKEN_ADDRESS = process.env.RECEIVE_TOKEN_ADDRESS;
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,5 @@
 import { createContractWallet, sendToken } from "@utils/aa/contractWallet";
+// import { createSafeWallet } from "./utils/aa/safe";
 
 const sleep = async (ms: number): Promise<void> => {
   return new Promise<void>((resolve) => {
@@ -12,12 +13,14 @@ export default function Home() {
 
   const createAAWallet = async () => {
 
-    const ecdsaProvider = await createContractWallet("000000000008");
-    console.log(
-      `contractWalletAddress: ${await ecdsaProvider.getAddress()}`
-    );
-    await sleep(3000);
-    await sendToken(ecdsaProvider);
+    // const ecdsaProvider = await createContractWallet("000000000008");
+    // console.log(
+    //   `contractWalletAddress: ${await ecdsaProvider.getAddress()}`
+    // );
+    // await sleep(3000);
+    // await sendToken(ecdsaProvider);
+    console.log(`createAAWallet`);
+    await createSafeWallet("000000000000");
   }
 
   createAAWallet()
