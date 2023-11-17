7d186ee25df4cc9ccad03538c098c16295acbbfe
ponponnsan <2469suhara@gmail.com>
2023-11-17 11:22:09 +0900

    comment out notification sound

--- src/app/app/components/recognizevoice/notificationSound.tsx
+++ src/app/app/components/recognizevoice/notificationSound.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import useSound from 'use-sound';
-// import compliteSound from "../../../public/itemget4.mp3"
+// import React from 'react';
+// // import useSound from 'use-sound';
+// // import compliteSound from "../../../public/itemget4.mp3"
 
-const SoundComponent = () => {
-  const [play, { stop }] = useSound("../../../public/itemget4.mp3");
+// const SoundComponent = () => {
+//   const [play, { stop }] = useSound("../../../public/itemget4.mp3");
 
-  return (
-    <div>
-      <button onClick={play}>Play Sound</button>
-      <button onClick={stop}>Stop Sound</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <button onClick={play}>Play Sound</button>
+//       <button onClick={stop}>Stop Sound</button>
+//     </div>
+//   );
+// };
 
-export default SoundComponent;
+// export default SoundComponent;
