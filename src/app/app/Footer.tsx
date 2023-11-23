7460835bacc053b8a20f5dd4699185119621f70c
ponponnsan <2469suhara@gmail.com>
2023-11-23 10:59:35 +0900

    newUI

--- src/app/app/Footer.tsx
+++ src/app/app/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
     <footer className="flex justify-around items-center px-4 py-2 fixed inset-x-0 bottom-0 bg-white border-t border border-gray-300 z-5 rounded-full">
     <Link href="/history" passHref>
       <div className="flex flex-col items-center text-center hover:bg-green-200 dark:hover:bg-gray-200 p-2 rounded-full transition-all duration-300 text-gray-400">
-        <Image src="/history.svg" alt="historyIcon" width={20} height={20} />
+        <Image src="/handshake-outline.png" alt="historyIcon" width={20} height={20} />
         <span>history</span>
       </div>
     </Link>
