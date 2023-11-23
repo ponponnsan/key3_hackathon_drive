5c7487e194989794fb0156a4426841164b37e848
ponponnsan <2469suhara@gmail.com>
2023-11-23 10:55:07 +0900

    newUI

--- src/app/app/components/utils/button/ActionButton.tsx
+++ src/app/app/components/utils/button/ActionButton.tsx
@@ -5,12 +5,17 @@ import { ActionButtonProps } from '@/app/utils/interfaces';
 const ActionButton: React.FC<ActionButtonProps> = ({ mainText, subText, onClick, style }) => {
   const [hover, setHover] = useState(false);
 
-  const hoverStyle = {
-    backgroundColor: hover ? '#ccffcc' : '#FFFFFF',
-  };
+  let hoverStyle = {};
+  if (style) {
+    hoverStyle = {
+      ...style,
+      backgroundColor: hover ? '#ccffcc' : '#FFFFFF', // ホバー時にはライトグリーン、そうでなければ白
+    };
+  }
+
   return (
     <div className="m-4">
-      <button className="bg-green-100 hover:bg-green-200 dark:bg-white dark:border-2 dark:text-black dark:hover:bg-gray-200 w-full p-4 rounded-lg font-semibold transition-colors duration-150" onClick={onClick}       style={hoverStyle}
+      <button className="bg-green-100 hover:bg-green-200 dark:bg-white dark:border-2 dark:text-black dark:hover:bg-gray-200 w-full p-4 rounded-lg font-semibold transition-colors duration-150" onClick={onClick} style={hoverStyle}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}>
         {mainText}
